66b0f96f86e8b0899c989f6a1a133fc38c253e5a
seabeePraveen <cbsrirangapraveen.2004@gmail.com>
2024-04-27 17:00:56 +0530

    resolved the issue with env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.APIKEY': JSON.stringify(env.APIKEY),
+      'process.env.AUTHDOMAIN': JSON.stringify(env.AUTHDOMAIN),
+      'process.env.PROJECTID': JSON.stringify(env.PROJECTID),
+      'process.env.STORAGEBUCKET': JSON.stringify(env.STORAGEBUCKET),
+      'process.env.MESSAGINGSENDERID': JSON.stringify(env.MESSAGINGSENDERID),
+      'process.env.APPID': JSON.stringify(env.APPID),
+      'process.env.MEASUREMENTID': JSON.stringify(env.MEASUREMENTID),
+    },
+    plugins: [react()],
+  }
 })
